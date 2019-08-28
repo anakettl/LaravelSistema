cfa7cc1d4b2e9d2c5195b9e49fa08d5b81e2bd65
Ana Clara Kettl <anakettl@gmail.com>
2019-08-28 09:44:08 -0300

    inclusao do font awesome e melhoria de design da view

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -20,13 +20,16 @@ Séries
 		<ul class="list-group">
 		<?php
 			foreach ($series as $serie ): ?> 			
-			<li class="list-group-item"> {{$serie->nome}}
+			<li class="list-group-item d-flex justify-content-between align-items-center"> {{$serie->nome}}
 			<form method="post" action="/LaravelSistema/public/series/delete/{{$serie->id}}"
 				onsubmit="return confirm ('Deseja remover {{addslashes($serie->nome)}}')">
 				<!--addslashes é uma função do php para retornar uma string-->
 				@csrf
 				@method('DELETE')
-				<button class="btn btn-danger">Excluir</button>
+				<button class="btn btn-danger btn-sm">
+					<i class="far fa-trash-alt"></i><!--colocada imagem do font awesome no lugar da palavra excluir-->
+				</button>
+				
 				
 			</form>
 			 </li>		<!--é possivel deixar dentro de duas chaves q o blade trata como codigo php-->	
--- resources/views/series/layout.blade.php
+++ resources/views/series/layout.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> <!--bootstrap-->
-
+	<script src="https://kit.fontawesome.com/ac9c854343.js"></script>
 </head>
 <body>
 	<div class="container">
